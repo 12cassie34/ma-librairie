bee61b508de6acf446d3ccf84379c18a82c203f3
Shan Yu Lin <ineslin35@gmail.com>
2021-07-11 23:15:37 +0800

    Successfully install uikit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
     <sidebar></sidebar>
+    <nav class="uk-navbar-container" uk-navbar>
+      <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+          <li class="uk-active"><a href="#">Active</a></li>
+          <li>
+            <a href="#">Parent</a>
+            <div class="uk-navbar-dropdown">
+              <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-active"><a href="#">Active</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+              </ul>
+            </div>
+          </li>
+          <li><a href="#">Item</a></li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-import sidebar from '@/components/Sidebar.vue';
+
+import sidebar from "@/components/Sidebar.vue";
 
 export default {
   components: { sidebar },
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/uikit/src/scss/variables-theme.scss";
+@import "../node_modules/uikit/src/scss/mixins-theme.scss";
+@import "../node_modules/uikit/src/scss/uikit.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+UIkit.use(Icons);
+
 Vue.config.productionTip = false;
 
 new Vue({
