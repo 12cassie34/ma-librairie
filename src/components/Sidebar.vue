941c9cdaf7198ef63eca8dc37c719e3b19f5782e
Shan Yu Lin <ineslin35@gmail.com>
2021-07-13 21:38:35 +0800

    Add sidebar css style

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="nav">
-    <a href="#offcanvas-slide" class="uk-button uk-button-default" uk-toggle>Open</a>
-
-    <div id="offcanvas-slide" class="uk-offcanvas uk-open" uk-offcanvas>
-      <div class="uk-width-1-2@s">
-        <ul class="uk-nav-primary uk-nav-parent-icon" uk-nav>
-          <li class="uk-active"><router-link to="/">現正閱讀</router-link></li>
-          <li class="uk-parent">
-            <a href="#">已閱讀書單</a>
-            <ul class="uk-nav-sub">
-              <li><a href="#">2021</a></li>
-              <li class="disabled"><a href="#">2022</a></li>
-            </ul>
-          </li>
-          <li class="uk-active"><router-link to="/">願望書單</router-link></li>
-        </ul>
-      </div>
+    <div class="list-container">
+      <ul class="uk-nav-primary uk-nav-parent-icon" uk-nav>
+        <li class="uk-active"><router-link to="/">現正閱讀</router-link></li>
+        <li class="uk-parent">
+          <a href="#">已閱讀書單</a>
+          <ul class="uk-nav-sub">
+            <li><a href="#">2021</a></li>
+            <li class="disabled"><a href="#">2022</a></li>
+          </ul>
+        </li>
+        <li class="uk-active">
+          <router-link to="/wish-list">願望書單</router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#nav {
+  width: 15%;
+  background-color: $midnight;
+  .list-container {
+    padding: 1rem;
+    a {
+      font-size: 1.2rem;
+      color: $beach;
+    }
+    a.router-link-exact-active {
+      color: $pink;
+    }
+  }
+}
 </style>
