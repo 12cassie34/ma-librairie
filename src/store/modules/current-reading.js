f6e58c23b41ef1eec91e95abca2de6c248e63ac7
Shan Yu Lin <ineslin35@gmail.com>
2021-07-25 21:55:21 +0800

    Add currentReading data

--- src/store/modules/current-reading.js
+++ src/store/modules/current-reading.js
@@ -1,15 +1,30 @@
-import axios from "axios";
+// import axios from "axios";
+import currentReading from "@/json/currentReading.js";
 
 export default {
     namespaced: true,
-    state: {},
+    state: {
+        name: "",
+        trans_name: "",
+        author: "",
+        translator: "",
+        publish_house: "",
+        tw_publish_date: "",
+        pages: "",
+        current_page: ""
+    },
     getters: {},
     mutations: {},
     actions: {
         getData({state}) {
-            axios.get('/static/currentReading.json').then((response) => {
-                console.log(state, response);
-            })
+            state.name = currentReading.name;
+            state.trans_name = currentReading.trans_name;
+            state.author = currentReading.author;
+            state.translator = currentReading.translator;
+            state.publish_house = currentReading.publish_house;
+            state.tw_publish_date = currentReading.tw_publish_date;
+            state.pages = currentReading.pages;
+            state.current_page = currentReading.current_page;
         }
     }
 }
