811cb18d52527347410e6a57ef8562921c31ac0d
Shan Yu Lin <ineslin35@gmail.com>
2021-08-08 20:37:22 +0800

    Change CSS animation to Vue build-in transition

---
+++ src/components/StartReading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="container">
+            <h1>開啟新的閱讀旅程</h1>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    margin-top: 25vh;
+    // opacity: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,75 @@
 <template>
   <div class="home main-content">
-    <div id="current-reading" class="container">
-      <div class="progress">
-        <div class="bars">
-          <div :style="progressWidth" class="progress-bar"></div>
-          <div class="bar"></div>
+    <transition name="fade">
+      <div v-if="hasBook === true" id="current-reading" class="container">
+        <div class="progress">
+          <div class="bars">
+            <div :style="progressWidth" class="progress-bar"></div>
+            <div class="bar"></div>
+          </div>
+          <div class="number">{{ current_page }} / {{ pages }}</div>
         </div>
-        <div class="number">{{ current_page }} / {{ pages }}</div>
-      </div>
-      <div class="row">
-        <div class="col-6 img">
-          <img :src="require(`@/assets/${img}`)" :alt="trans_name">
-        </div>
-        <div class="col-6">
-          <div class="text-container">
-            <h2>{{ trans_name }}</h2>
-            <p>{{ name }}</p>
-            <p>{{ author }} / {{ translator }}</p>
+        <div class="row">
+          <div class="col-6 img">
+            <img :src="require(`@/assets/${img}`)" :alt="trans_name" />
+          </div>
+          <div class="col-6">
+            <div class="text-container">
+              <h2>{{ trans_name }}</h2>
+              <p>{{ name }}</p>
+              <p>{{ author }} / {{ translator }}</p>
+            </div>
           </div>
         </div>
+        <div class="btns">
+          <finish-reading />
+          <update-progress />
+        </div>
       </div>
-      <div class="btns">
-        <finish-reading />
-        <update-progress />
-      </div>
-    </div>
+      <start-reading v-if="hasBook === false" />
+    </transition>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import updateProgress from '../components/UpdateProgress.vue';
-import finishReading from '../components/FinishReading.vue';
+import { mapState } from "vuex";
+import updateProgress from "../components/UpdateProgress.vue";
+import finishReading from "../components/FinishReading.vue";
+import startReading from "../components/StartReading.vue";
 
 export default {
   name: "Home",
   components: {
-    updateProgress, finishReading
+    updateProgress,
+    finishReading,
+    startReading,
   },
   computed: mapState({
     // arrow functions can make the code very succinct!
-    name: state => state.currentReading.name,
-    trans_name: state => state.currentReading.trans_name,
-    img: state => state.currentReading.img,
-    author: state => state.currentReading.author,
-    translator: state => state.currentReading.translator,
-    pages: state => state.currentReading.pages,
-    current_page: state => state.currentReading.current_page,
+    name: (state) => state.currentReading.name,
+    trans_name: (state) => state.currentReading.trans_name,
+    img: (state) => state.currentReading.img,
+    author: (state) => state.currentReading.author,
+    translator: (state) => state.currentReading.translator,
+    pages: (state) => state.currentReading.pages,
+    current_page: (state) => state.currentReading.current_page,
+    hasBook: (state) => state.hasBook.hasBook,
     progressWidth() {
-      const percentage = this.current_page * 100 / this.pages;
+      const percentage = (this.current_page * 100) / this.pages;
       return {
-        width: `${percentage}%`
-      }
-    }
+        width: `${percentage}%`,
+      };
+    },
   }),
   watch: {
     current_page() {
       if (this.current_page === this.pages) {
-        this.$store.commit('hasBook/finishReading');
-        document.getElementById('current-reading').classList.add('finish');
+        this.$store.commit("hasBook/finishReading");
       }
-    }
+    },
   },
   created() {
-    this.$store.dispatch('currentReading/getData');
+    this.$store.dispatch("currentReading/getData");
   },
 };
 </script>
@@ -99,17 +105,17 @@ export default {
     height: 260px;
     background-color: $marron;
     .col-6.img {
-        position: relative;
-        img {
-          position: absolute;
-          top: 7%;
-          right: 25%;
-        }
+      position: relative;
+      img {
+        position: absolute;
+        top: 7%;
+        right: 25%;
+      }
     }
     .text-container {
-        padding-right: 85px;
-        padding-top: 30px;
-        color: ghostwhite;
+      padding-right: 85px;
+      padding-top: 30px;
+      color: ghostwhite;
     }
   }
   .btns {
@@ -128,8 +134,12 @@ export default {
     width: 200px;
   }
 }
-.container.finish {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 3s;
+}
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 </style>
